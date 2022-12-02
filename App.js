45eec1ed2a3b9b5a5f2689c9751bfa83488c9074
Ravindra kumawat <53889917+Ravindra-kumawat@users.noreply.github.com>
2022-12-02 09:58:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
+function App() {
+
+  console.log("app")
+  const { isLoaded } = useLoadScript({ googleMapsApiKey: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", });
+  if (!isLoaded) return <div className="App">Loading...</div>
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  );
+}
+
+const Map = () => {
+  return (
+    <GoogleMap zoom={10} center={{ lat: 44, lng: -80 }}>
+      <Marker position={{ lat: 44, lng: -80 }} />
+    </GoogleMap>
+  )
+}
+
+export default App;
